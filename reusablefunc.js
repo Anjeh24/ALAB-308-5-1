cce457bd7bfb8b643d369c622c866a282013c2b1
anjeh24 <junez24@yahoo.com>
2024-06-27 21:11:18 -0400

    adding function to calculate average, it works

--- reusablefunc.js
+++ reusablefunc.js
@@ -1,4 +1,4 @@
-
+//Part 1
 //function that returns the sum of an array of numbers
 let sum = 0;
 function theSum(){
@@ -9,4 +9,15 @@ function theSum(){
     }
     return sum;
 }
-console.log(theSum());
+console.log(theSum());//15
+
+let tot = 0;
+function theAvg(){
+    const arrey2 = [2, 3, 5, 6];
+    for (j = 0; j < arrey2.length; j++){
+        tot += arrey2[j];
+        
+    }
+    return tot / arrey2.length;
+}
+console.log(theAvg());//4
